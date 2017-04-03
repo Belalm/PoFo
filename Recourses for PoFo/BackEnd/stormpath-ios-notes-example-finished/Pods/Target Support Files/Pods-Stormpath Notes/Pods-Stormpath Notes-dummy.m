b00476af3b844799766c2066669ee9edbf633252
Belal El Monajjed <belal.elmonajjed@ubanquity.com>
2017-04-03 10:59:12 +0300

    Remove resource folder

--- Recourses for PoFo/BackEnd/stormpath-ios-notes-example-finished/Pods/Target Support Files/Pods-Stormpath Notes/Pods-Stormpath Notes-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Stormpath_Notes : NSObject
-@end
-@implementation PodsDummy_Pods_Stormpath_Notes
-@end
--- Recourses for PoFo/BackEnd/stormpath-ios-notes-example-finished/Pods/Target Support Files/Pods-Stormpath Notes/Pods-Stormpath Notes-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_Stormpath_NotesVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Stormpath_NotesVersionString[];
-
